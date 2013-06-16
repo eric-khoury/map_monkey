fe66403158f4ff24f2bcde8494b2dc5ce0d68c74
Eric Khoury <khoury@live.se>
2013-06-16 22:26:12 +0200

    Fixed indentation

--- lib/map_monkey.rb
+++ lib/map_monkey.rb
@@ -3,65 +3,65 @@ require 'rexml/document'
 
 class Position
 
-	attr_accessor :city, :street, :zip
+  attr_accessor :city, :street, :zip
 
-	def initialize(city = "", street = "", zip = "")
-		@city = city(city)
-		@street = street(street)
-		@zip = zip(zip)
+  def initialize(city = "", street = "", zip = "")
+    @city = city(city)
+    @street = street(street)
+    @zip = zip(zip)
+  end
+
+  def city(city)
+    URI.encode(city)
+  end
+
+  def street(street)
+    URI.encode(street)
+  end
+
+  def zip(zip)
+    URI.encode(zip)
 	end
 
-	def city(city)
-		URI.encode(city)
-	end
+  def lat
+    url = "http://maps.googleapis.com/maps/api/geocode/xml?address=#{@street}#{@zip}#{@city}&sensor=false"
+    xml_data = Net::HTTP.get_response(URI.parse(url)).body
+    doc = REXML::Document.new(xml_data)
 
-	def street(street)
-		URI.encode(street)
-	end
+    doc.elements.each('GeocodeResponse/result/geometry/location/lat') do |ele|
+      self.lat = ele.text
+    end
 
-	def zip(zip)
-		URI.encode(zip)
-	end
+    lat
+  end
 
-	def lat
-		url = "http://maps.googleapis.com/maps/api/geocode/xml?address=#{@street}#{@zip}#{@city}&sensor=false"
-		xml_data = Net::HTTP.get_response(URI.parse(url)).body
-		doc = REXML::Document.new(xml_data)
+  def lng
+    url = "http://maps.googleapis.com/maps/api/geocode/xml?address="+self.street+self.zip+self.city+"&sensor=false"
+    xml_data = Net::HTTP.get_response(URI.parse(url)).body
+    doc = REXML::Document.new(xml_data)
 
-		doc.elements.each('GeocodeResponse/result/geometry/location/lat') do |ele|
-   			self.lat = ele.text
-		end
+    doc.elements.each('GeocodeResponse/result/geometry/location/lng') do |ele|
+      lng = ele.text
+    end
 
-		lat
-	end
+    lng
+  end
 
-	def lng
-		url = "http://maps.googleapis.com/maps/api/geocode/xml?address="+self.street+self.zip+self.city+"&sensor=false"
-		xml_data = Net::HTTP.get_response(URI.parse(url)).body
-		doc = REXML::Document.new(xml_data)
+  def lat_lng
+    url = "http://maps.googleapis.com/maps/api/geocode/xml?address="+self.street+self.zip+self.city+"&sensor=false"
+    xml_data = Net::HTTP.get_response(URI.parse(url)).body
+    doc = REXML::Document.new(xml_data)
 
-		doc.elements.each('GeocodeResponse/result/geometry/location/lng') do |ele|
-   			lng = ele.text
-		end
+    doc.elements.each('GeocodeResponse/result/geometry/location/lat') do |ele|
+      lat = ele.text
+    end
 
-		lng
-	end
+    doc.elements.each('GeocodeResponse/result/geometry/location/lng') do |ele|
+      lng = ele.text
+    end
 
-	def lat_lng
-		url = "http://maps.googleapis.com/maps/api/geocode/xml?address="+self.street+self.zip+self.city+"&sensor=false"
-		xml_data = Net::HTTP.get_response(URI.parse(url)).body
-		doc = REXML::Document.new(xml_data)
+    lat_lng = [lat, lng]
 
-		doc.elements.each('GeocodeResponse/result/geometry/location/lat') do |ele|
-   			lat = ele.text
-		end
-
-		doc.elements.each('GeocodeResponse/result/geometry/location/lng') do |ele|
-   			lng = ele.text
-		end
-
-		lat_lng = [lat, lng]
-
-		lat_lng
-	end
+    lat_lng
+  end
 end
