1be3c3f3f1a8387c5edd54ac9de4e74a7deed903
Eric Khoury <khoury@live.se>
2013-06-26 20:43:30 +0200

    Fixed lat method. Fetching initialize params.

--- lib/map_monkey/position.rb
+++ lib/map_monkey/position.rb
@@ -3,69 +3,68 @@ require 'rexml/document'
 
 module MapMonkey
   class Position
+    attr_accessor :city, :street, :zip, :lat, :lng
 
-  attr_accessor :city, :street, :zip
-
-  def initialize(options = {})
-    @city = URI.encode(options[:city])
-    @street = URI.encode(options[:street])
-    @zip = URI.encode(options[:zip])
-  end
-
-  def city
-    URI.encode(@city)
-  end
-
-  def street
-    URI.encode(@street)
-  end
-
-  def zip
-    URI.encode(@zip)
-	end
-
-  def url
-    "http://maps.googleapis.com/maps/api/geocode/xml?address=#{street}#{zip}#{city}&sensor=false"
-  end
-
-  def get_data
-    Net::HTTP.get_response(URI.parse(url)).body
-  end
-
-  def lat
-    doc = REXML::Document.new(get_data)
-
-    doc.elements.each('GeocodeResponse/result/geometry/location/lat') do |ele|
-      self.lat = ele.text
+    def initialize(options = {})
+      @city   = URI.encode(options.fetch(:city, ""))
+      @street = URI.encode(options.fetch(:street, ""))
+      @zip    = URI.encode(options.fetch(:zip, ""))
     end
 
-    lat
-  end
-
-  def lng
-    doc = REXML::Document.new(get_data)
-
-    doc.elements.each('GeocodeResponse/result/geometry/location/lng') do |ele|
-      lng = ele.text
+    def city
+      URI.encode(@city)
     end
 
-    lng
-  end
-
-  def lat_lng
-    doc = REXML::Document.new(get_data)
-
-    doc.elements.each('GeocodeResponse/result/geometry/location/lat') do |ele|
-      lat = ele.text
+    def street
+      URI.encode(@street)
     end
 
-    doc.elements.each('GeocodeResponse/result/geometry/location/lng') do |ele|
-      lng = ele.text
+    def zip
+      URI.encode(@zip)
     end
 
-    lat_lng = [lat, lng]
+    def url
+      "http://maps.googleapis.com/maps/api/geocode/xml?address=#{street}#{zip}#{city}&sensor=false"
+    end
 
-    lat_lng
-  end
+    def get_data
+      Net::HTTP.get_response(URI.parse(url)).body
+    end
+
+    def get_lat
+      doc = REXML::Document.new(get_data)
+
+      doc.elements.each('GeocodeResponse/result/geometry/location/lat') do |ele|
+        @lat = ele.text
+      end
+
+      @lat
+    end
+
+    def get_lng
+      doc = REXML::Document.new(get_data)
+
+      doc.elements.each('GeocodeResponse/result/geometry/location/lng') do |ele|
+        @lng = ele.text
+      end
+
+      @lng
+    end
+
+    def get_lat_lng
+      doc = REXML::Document.new(get_data)
+
+      doc.elements.each('GeocodeResponse/result/geometry/location/lat') do |ele|
+        @lat = ele.text
+      end
+
+      doc.elements.each('GeocodeResponse/result/geometry/location/lng') do |ele|
+        @lng = ele.text
+      end
+
+      lat_lng = [lat, lng]
+
+      lat_lng
+    end
   end
 end
