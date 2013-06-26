da1f0dd2391f9ab97f2adf21c98603b31d19af4f
Eric Khoury <khoury@live.se>
2013-06-26 20:43:56 +0200

    Tests

--- spec/position_spec.rb
+++ spec/position_spec.rb
@@ -3,14 +3,26 @@ require 'map_monkey'
 
 describe "position" do
   it "can assign city" do
-    pos = MapMonkey::Position.new(:city => "gothenburg", :street => "kabelgatan", :zip => "41457")
+    pos = MapMonkey::Position.new(city: "gothenburg")
 
     pos.city.should eq "gothenburg"
   end
 
-  it "can get lat coordinate" do
-    pos = MapMonkey::Position.new(:city => "gothenburg", :street => "kabelgatan", :zip => "41457")
+  it "can assign street" do
+    pos = MapMonkey::Position.new(street: "kabelgatan")
 
-    pos.should respond_to(:lat)
+    pos.street.should eq "kabelgatan"
+  end
+
+  it "can assign zip" do
+    pos = MapMonkey::Position.new(zip: "41457")
+
+    pos.zip.should eq "41457"
+  end
+
+  it "can get lat coordinate" do
+    pos = MapMonkey::Position.new(city: "gothenburg", street: "kabelgatan", zip: "41457")
+
+    pos.get_lat.should == "something else"
   end
 end
