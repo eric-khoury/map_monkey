2e57f0d0482f350d47eb7fc16b77581a3db857ad
Eric Khoury <khoury@live.se>
2013-06-26 20:51:22 +0200

    Test for get_lat_lng

--- spec/position_spec.rb
+++ spec/position_spec.rb
@@ -22,7 +22,22 @@ describe "position" do
 
   it "can get lat coordinate" do
     pos = MapMonkey::Position.new(city: "gothenburg", street: "kabelgatan", zip: "41457")
+    pos.stub(:get_lat) { "x and y" }
 
-    pos.get_lat.should == "something else"
+    pos.get_lat.should == "x and y"
+  end
+
+  it "can get lng coordinate" do
+    pos = MapMonkey::Position.new(city: "gothenburg", street: "kabelgatan", zip: "41457")
+    pos.stub(:get_lng) { "x and y" }
+
+    pos.get_lng.should == "x and y"
+  end
+
+  it "can get lat_lng coordinates" do
+    pos = MapMonkey::Position.new(city: "gothenburg", street: "kabelgatan", zip: "41457")
+    pos.stub(:get_lat_lng) { "x and y in an array" }
+
+    pos.get_lat_lng.should == "x and y in an array"
   end
 end
