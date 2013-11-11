fa0245ba50da48ceec57d20a0267065cd8aaf352
Eric Khoury <khoury@live.se>
2013-11-11 08:23:31 +0100

    Refactoring

--- spec/position_spec.rb
+++ spec/position_spec.rb
@@ -2,39 +2,31 @@ require 'rspec'
 require 'map_monkey'
 
 describe "position" do
-  it "can assign city" do
-    pos = MapMonkey::Position.new(city: "gothenburg")
+  before do
+    @pos = MapMonkey::Position.new(city: "gothenburg", street: "kabelgatan", zip: "41457")
+  end
 
-    pos.city.should eq "gothenburg"
+  it "can assign city" do
+    @pos.city.should eq "gothenburg"
   end
 
   it "can assign street" do
-    pos = MapMonkey::Position.new(street: "kabelgatan")
-
-    pos.street.should eq "kabelgatan"
+    @pos.street.should eq "kabelgatan"
   end
 
   it "can assign zip" do
-    pos = MapMonkey::Position.new(zip: "41457")
-
-    pos.zip.should eq "41457"
+    @pos.zip.should eq "41457"
   end
 
   it "can get lat coordinate" do
-    pos = MapMonkey::Position.new(city: "gothenburg", street: "kabelgatan", zip: "41457")
-
-    pos.get_lat.should == "57.6892275"
+    @pos.get_lat.should == "57.6892275"
   end
 
   it "can get lng coordinate" do
-    pos = MapMonkey::Position.new(city: "gothenburg", street: "kabelgatan", zip: "41457")
-
-    pos.get_lng.should == "11.9253258"
+    @pos.get_lng.should == "11.9253258"
   end
 
   it "can get lat_lng coordinates" do
-    pos = MapMonkey::Position.new(city: "gothenburg", street: "kabelgatan", zip: "41457")
-
-    pos.get_lat_lng.should == ["57.6892275", "11.9253258"]
+    @pos.get_lat_lng.should == ["57.6892275", "11.9253258"]
   end
 end
